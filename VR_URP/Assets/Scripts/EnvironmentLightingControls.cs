9754c3f474312c48156cfbfd83db561ec3bd6702
JacobPZO <156956701+JacobPZO@users.noreply.github.com>
2026-02-22 17:00:21 -0700

    Environ + materials panel

---
+++ VR_URP/Assets/Scripts/EnvironmentLightingControls.cs
@@ -0,0 +1,47 @@
+using static System.Net.Mime.MediaTypeNames;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class EnvironmentLightingControls : MonoBehaviour
+{
+    public Toggle useSkyboxToggle;
+    public Slider skyboxIntensitySlider;
+    public UnityEngine.UI.Text skyboxIntensityText;
+    public Toggle enableFog;
+
+    public bool EnableFog
+    {
+        get => RenderSettings.fog;
+        set => RenderSettings.fog = value;
+    }
+
+    public bool UseSkybox
+    {
+        get => (RenderSettings.ambientMode == AmbientMode.Skybox);
+        set
+        {
+            RenderSettings.ambientMode = (value) ?
+                AmbientMode.Skybox : AmbientMode.Flat;
+            skyboxIntensitySlider.interactable = value;
+            skyboxIntensityText.gameObject.SetActive(value);
+        }
+    }
+
+    public float SkyboxIntensity
+    {
+        get => RenderSettings.ambientIntensity;
+        set
+        {
+            RenderSettings.ambientIntensity = value;
+            skyboxIntensityText.text = value.ToString("F1");
+        }
+    }
+
+    private void Start()
+    {
+        useSkyboxToggle.isOn = UseSkybox;
+        skyboxIntensitySlider.value = SkyboxIntensity;
+        enableFog.isOn = EnableFog;
+    }
+}
